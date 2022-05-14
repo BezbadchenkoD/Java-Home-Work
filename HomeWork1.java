cb871e4888f8f2d898cc4c7c8b8f51a9d6ae37ad
BezbadchenkoD <BezbadchenkoDm@gmail.com>
2022-05-14 12:39:32 +0700

    Create HomeWork1.java

---
+++ HomeWork1.java
@@ -0,0 +1,12 @@
+public class HomeWork1 {
+    public static void main(String[] args){
+        int age = 20;
+        String name = "Dima";
+        int amount = 50000;
+        double rate = 2.5;
+
+        System.out.println("You will save " + (amount * rate * (60 - age) - amount) +"!");
+
+    }
+
+}
